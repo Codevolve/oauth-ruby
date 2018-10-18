22178f1a7a019ee05389d0842d66ef90786d3b97
Saul Costa <saul@codevolve.com>
2018-10-18 11:39:33 -0700

    Create action_dispatch_request.rb

---
+++ lib/oauth/request_proxy/action_dispatch_request.rb
@@ -0,0 +1,6 @@
+require 'oauth/request_proxy/rack_request'
+ module OAuth::RequestProxy
+  class ActionDispatchRequest < OAuth::RequestProxy::RackRequest
+    proxies ActionDispatch::Request
+  end
+end
